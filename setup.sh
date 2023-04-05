86e42f188ebe2bb3ba3a1439665330d49ca8210f
Paweł Kołodziejski <aquadran@gmail.com>
2023-04-05 22:47:48 +0200

    re-enabled xz-native build

--- setup.sh
+++ setup.sh
@@ -429,9 +429,9 @@ DISTRO = \"${DISTRO}\"
 INHERIT = \"rm_work\"
 BUILD_DEBUG = \"${BUILD_DEBUG}\"
 ASSUME_PROVIDED += \" git-native perl-native python-native python3-native python3-mako-native \
-desktop-file-utils-native linux-libc-headers-native glib-2.0-native intltool-native xz-native \
-gzip-native findutils-native bison-native flex-native help2man-native bc-native subversion-native \
-m4-native unzip-native texinfo-native texinfo-dummy-native patch-replacement-native makedepend-native \
+desktop-file-utils-native linux-libc-headers-native glib-2.0-native intltool-native gzip-native \
+findutils-native bison-native flex-native help2man-native bc-native subversion-native m4-native \
+unzip-native texinfo-native texinfo-dummy-native patch-replacement-native makedepend-native \
 chrpath-replacement-native meson-native ninja-native cmake-native rsync-native zstd-native\"
 SANITY_REQUIRED_UTILITIES:remove = \"chrpath\"
 PACKAGE_DEPENDS:remove = \"dwarfsrcfiles-native pseudo-native\"
