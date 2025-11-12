6de1e9597119066c2e74dc6d5b7b2ac0f6121356
Paweł Kołodziejski <aquadran@gmail.com>
2025-11-12 22:34:58 +0100

    added ti64 kernel 6.12

---
+++ meta-ti/recipes-kernel/linux/linux-ti64_6.12/byteswap.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
+#ifndef __BYTE_SWAP_H
+#define __BYTE_SWAP_H
+
+#define bswap_16 __builtin_bswap16
+#define bswap_32 __builtin_bswap32
+#define bswap_64 __builtin_bswap64
+
+#endif
+
